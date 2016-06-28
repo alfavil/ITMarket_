d0081526616ad0eb866d6d6bcb0ccad2b90d7183
alfavil <kamran.wil29@gmail.com>
2016-06-28 22:26:48 +0300

    I changed annotaition

--- src/main/java/servlets/IndexServlet.java
+++ src/main/java/servlets/IndexServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,12 +11,12 @@ import java.io.PrintWriter;
 /**
  * Created by kiumars-pc on 6/28/2016.
  */
+@WebServlet(urlPatterns ="/index")
 public class IndexServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        PrintWriter out=resp.getWriter();
-        out.print("<h1>Hello Servlet</h1>");
+req.getRequestDispatcher("index.jsp").forward(req,resp);
 
     }
 }
